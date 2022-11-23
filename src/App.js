fa9c51e59f064864fdd89df66b1a58c1ceb11f21
MohdAnas07 <anas98144@gmail.com>
2022-11-23 18:30:00 +0530

    assignment done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import Login from "./component/auth/Login";
+import Dashboard from './component/auth/Dashboard';
+import ForgotPassword from './component/auth/ForgotPassword';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+
+
+              // <Route exact path="/" component={Signup} />
+
+// <Route path="/signup" component={Signup} />
+              // <Route path="/forgot-password" component={ForgotPassword} />
---
+++ src/component/auth/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <div className="dashboard">
+            <h1>Welcome to <span style={{ color: "red" }} >TOP LEARNER</span> DashBoard </h1>
+            <h2>➖❤️➖</h2>
+        </div>
+    )
+}
+
+export default Dashboard
